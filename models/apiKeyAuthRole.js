0fcf9d0ba8014e66a80065f754643c25683ff141
Scott Kornblatt <skornblatt@rtm.com>
2017-04-09 13:07:43 -0400

    fixed error

--- models/apiKeyAuthRole.js
+++ models/apiKeyAuthRole.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    apiKey { type: Schema.Types.ObjectId, ref: 'apiKey', required: true },
+    apiKey: { type: Schema.Types.ObjectId, ref: 'apiKey', required: true },
     role: {type: String, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
